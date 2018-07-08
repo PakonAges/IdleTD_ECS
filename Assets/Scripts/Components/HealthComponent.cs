2a38288567a4aaf830d7d869a7952f1479e4a207
Max Okorokov <maxim.okorokov@gmail.com>
2018-07-08 14:56:09 +0300

    Health component added

---
+++ Assets/Scripts/Components/HealthComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Unity.Entities;
+
+[Serializable]
+public struct Health : IComponentData {
+    public int Value;
+}
+
+class HealthComponent : ComponentDataWrapper<Health> {
+
+}
